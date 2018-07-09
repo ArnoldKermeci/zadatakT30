63fc9c779af303db61d540ec9bdf8f8990904362
ArnoldKermeci <k.arnold111@gmail.com>
2018-07-09 13:40:34 +0200

    commit 2

--- app/src/main/java/com/example/arnold/myapp/db/ORMLightHelper.java
+++ app/src/main/java/com/example/arnold/myapp/db/ORMLightHelper.java
@@ -1,4 +1,73 @@
 package com.example.arnold.myapp.db;
 
-public class ORMLightHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.arnold.myapp.db.model.Actor;
+import com.example.arnold.myapp.db.model.Movie;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+public class ORMLightHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final String DATABASE_NAME    = "db";
+    private static final int    DATABASE_VERSION = 1;
+
+    private Dao<Movie, Integer> mProductDao = null;
+    private Dao<Actor, Integer> mActorDao = null;
+
+    public ORMLightHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, Movie.class);
+            TableUtils.createTable(connectionSource, Actor.class);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            TableUtils.dropTable(connectionSource, Movie.class, true);
+            TableUtils.dropTable(connectionSource, Actor.class, true);
+            onCreate(database, connectionSource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Dao<Movie, Integer> getMovieDao() throws SQLException {
+        if (mProductDao == null) {
+            mProductDao = getDao(Movie.class);
+        }
+
+        return mProductDao;
+    }
+
+    public Dao<Actor, Integer> getActorDao() throws SQLException {
+        if (mActorDao == null) {
+            mActorDao = getDao(Actor.class);
+        }
+
+        return mActorDao;
+    }
+
+    //obavezno prilikom zatvarnaj rada sa bazom osloboditi resurse
+    @Override
+    public void close() {
+        mProductDao = null;
+        mActorDao = null;
+
+        super.close();
+    }
 }
+
